fae8593f8547d89edae137c6a3fc5f092f7546f1
Yasser Shalabi <yshalab2@illinois.edu>
2019-06-21 23:50:09 +0000

    graph aws

---
+++ misc/contention-l1d/graph_aws.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+CONTENTION_BINS=/isca19/bins
+MEASURE_L1D_CON=${CONTENTION_BINS}/isca-cc-measure-l1d-contention
+MODE=points #2
+CORE=${TSEND}
+OTHER=${TRECV}
+while true
+do
+    taskset -c ${CORE} ${MEASURE_L1D_CON} > contention.dat
+    clear
+    cat contention.dat | gnuplot -p -e "set terminal dumb size 120, 30; set xlabel \"L1D Set Index (Shared with Thread-${OTHER})\"; set yrange [30:120]; plot '-' using 1:3 with ${MODE} title \"Thread-${CORE} L1D Access Time (cycles)\"";
+done
