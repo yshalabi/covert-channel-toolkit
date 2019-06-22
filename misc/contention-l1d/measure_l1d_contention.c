5f8447866b1ae90e2cecd3b8d5dd4b6059b6d518
Yasser Shalabi <yassershalabi@gmail.com>
2019-06-22 08:06:12 -0700

    attempted fix

--- misc/contention-l1d/measure_l1d_contention.c
+++ misc/contention-l1d/measure_l1d_contention.c
@@ -17,42 +17,40 @@
 #include <string.h>
 #include "l1.h"
 #include "low.h"
+#include "crc16.h"
 
+#define SIGNALING_THRESHOLD 120
 #define FIRST_CHAR_INDEX 2
 #define LAST_CHAR_INDEX 6
-#define SAMPLE_COUNT (1024*1024)
 
-uint64_t set_timings[64][256];
-void decode_thresholds(uint16_t * results, int * rmap)
+bool verify_crc16(char * p) 
 {
+    uint16_t crc = crc16_ccitt_init();
+    for(int i =0; i < 8; i++)
+        crc = crc16_ccitt_update(p[i], crc);
+    crc = crc16_ccitt_finalize(crc);
+    return (crc == 0);
+}
+
+int set_timings[64][256];
+void decode_thresholds(uint16_t * results, int * rmap, char * dst)
+{
+    char payload[8] = {0,0,0,0,0,0,0,0};
     int set_threshold[64];
-    int set_samples[64];
     for(int i = 0; i < 64; i++)
     {
-        set_samples[i] = 0;
-        set_threshold[i] = 0;
         for(int j = 0; j < 256; j++) set_timings[i][j] = 0;
-        for(int s = 0; s < SAMPLE_COUNT; s++) {
+        for(int s = 0; s < 256; s++) {
             int cycles = results[s*64 + rmap[i]];
-            if(cycles < 256 && cycles >5) {
+            if(cycles < 256 && cycles >0)
                 set_timings[i][cycles]++;
-                set_samples[i]++;
-            }
-        }
-        for(int c = 0; c < 256; c++)
-        {
-            if(set_timings[i][c] == 0) continue;
-            //printf("set_timings[set=%d][cycles=%d]=%d\n", i,c, set_timings[i][c]);
         }
     }
     for(int i = 0; i < 64; i++) {
-        uint64_t sum = 0;
-        int half = set_samples[i]/2;
-        //printf("HP Set(%d) = %d\n",i,half);
-        set_threshold[i] = 256;
+        int sum = 0;
         for(int j = 0; j < 256; j++) {
             sum+=set_timings[i][j];
-            if(sum < half)
+            if(sum < 128)
                 continue;
             set_threshold[i] = j;
             break;
@@ -60,17 +58,33 @@ void decode_thresholds(uint16_t * results, int * rmap)
     }
     int set = 0;
     for(int c = 0; c < 8; c++){
+        payload[c] = 0;
         for(int b = 0; b < 8; b++) {
             int i = c*8 + b;
-            printf("%d\tS%d\t%d\n", set, set++, set_threshold[i]);
+            //printf("SET %d = %d\n", set++, set_threshold[i]);
+            printf("%d\tS%d\t%d\n", i, i, set_threshold[i]);
+            if(set_threshold[i] > SIGNALING_THRESHOLD)
+                payload[c] |= (1<<b);
         }
     }
+    
+    dst[0] = payload[0];
+    dst[1] = payload[1];
+    dst[2] = payload[2];
+    dst[3] = payload[3];
+    dst[4] = payload[4];
+    dst[5] = payload[5];
+    dst[6] = payload[6];
+    dst[7] = payload[7];
 }
 
 int main(int argc, char **argv)
 {
     setbuf(stdout, NULL);
     char msg[7];
+    msg[6] = 0;
+    uint64_t next_period;
+    int i;
     l1pp_t l1 = l1_prepare();
     int nsets = l1_getmonitoredset(l1, NULL, 0);
     int *map = calloc(nsets, sizeof(int));
@@ -80,14 +94,18 @@ int main(int argc, char **argv)
     for(int i =0;i<64;i++) rmap[i] = -1;
     for(int i =0; i < nsets; i++) rmap[map[i]] = i;
 
-    uint16_t * results;
-    for(int r = 0; r < 12; r++)
-    {
-        int samples = SAMPLE_COUNT;
-        results = calloc(samples * nsets, sizeof(uint16_t));
-        for (int i = 0; i < samples * nsets; i+= 4096/sizeof(uint16_t))
-            results[i] = 1;
+    int samples = 256;
+    uint16_t *results = calloc(samples * nsets, sizeof(uint16_t));
+    for (int i = 0; i < samples * nsets; i+= 4096/sizeof(uint16_t))
+        results[i] = 1;
 
+    int next_msg = 0;
+    uint16_t last_packet_num = 0xFFFF;
+    char buf[128];
+    char * pos = buf;
+    while(1) {
+
+        char packet[8] = {0,0,0,0,0,0,0,0};
         for(int i = 0; i < 128; i++)
         {
             uint16_t * res = results + nsets*i*2;
@@ -96,10 +114,34 @@ int main(int argc, char **argv)
             l1_bprobe(l1, res);
         }
 
-        for(int d = 0; d < 1000*1000; d++) asm("pause");
-        
-    }
-    decode_thresholds(results,rmap);
+        decode_thresholds(results,rmap, packet);
+        return 0;
 
+        bool crc_is_valid   = verify_crc16(packet);
+        uint16_t packet_num = (uint16_t) packet[0];
+        bool is_new_packet  = packet_num != last_packet_num;
+
+        if(crc_is_valid && is_new_packet)
+            last_packet_num = packet_num;
+        else {
+            sched_yield();
+            continue;
+        }
+
+        //Move the packet contents into the buffer
+        for(int i = FIRST_CHAR_INDEX; i < LAST_CHAR_INDEX; i++ ) {
+            //A new line is the end of the message
+            if(packet[i] == '\n') {
+                pos[0] = 0;
+                pos = buf;
+                //Print buffer contents
+                printf("Msg: %s\n", pos);
+                memset(buf,0,128);
+                break;
+            } else {
+                *pos++ = packet[i];
+            }
+        }
+    }
     return 0;
 }
