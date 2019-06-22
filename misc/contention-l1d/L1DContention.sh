4c4634531392d44bba53c1c670f90591ef1f6ce5
Yasser Shalabi <yshalab2@illinois.edu>
2019-06-22 05:01:06 -0700

    Update L1DContention.sh

--- misc/contention-l1d/L1DContention.sh
+++ misc/contention-l1d/L1DContention.sh
@@ -6,7 +6,7 @@ CORE=${TSEND}
 OTHER=${TRECV}
 while true
 do
-    taskset -c ${CORE} ${MEASURE_L1D_CON} > contention.dat
+    ${MEASURE_L1D_CON} > contention.dat
     clear
     cat contention.dat | gnuplot -p -e "set terminal dumb size 120, 30; set xlabel \"L1D Set Index (Shared with Thread-${OTHER})\"; set yrange [30:120]; plot '-' using 1:3 with ${MODE} title \"Thread-${CORE} L1D Access Time (cycles)\"";
 done
